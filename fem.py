181f72fe2b41f234ad3cd9b90cfde0697e6d3a44
UnaSchmidt <hanacukovacevic@gmail.com>
2018-10-27 21:24:54 +0200

    Update fem.py
    En - 1
    Två - 2
    Tre - 3
    Fyra - 4
    Fem - 5

--- fem.py
+++ fem.py
@@ -1,3 +1,8 @@
 #print the data type of "a=10" using a built-in Python function
 a=10
 print(type(a))
+
+#JK (Not Jelena Karleuša, not just kidding) [script kidding]
+
+#jobs: list active jobs
+#join: join lines on a common field
