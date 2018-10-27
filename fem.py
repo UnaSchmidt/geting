c09aafa53e1f8b1b536d3ebd87d5de1cf13dff06
UnaSchmidt <hanacukovacevic@gmail.com>
2018-10-27 21:19:06 +0200

    JUST UPDATE IT OMFG ALREADY
    This is an optional extended file description. Sorry for being aggressive.

--- fem.py
+++ fem.py
@@ -1,3 +1,10 @@
 #print the data type of "a=10" using a built-in Python function
 a=10
 print(type(a))
+
+#wow omfg line 3 does something semi-important, like tell me a datatype, so I don't have to learn them OMFG wooowwwwwwwwww
+
+
+
+
+#still, I gotta know 'em all
